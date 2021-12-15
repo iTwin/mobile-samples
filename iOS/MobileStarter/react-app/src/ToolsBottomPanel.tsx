02b152c250a1346bade8ad18a53ed692d92bcfba
Travis.Cobbs <>
2021-12-15 14:59:48 -0800

    Add section tools.

--- iOS/MobileStarter/react-app/src/ToolsBottomPanel.tsx
+++ iOS/MobileStarter/react-app/src/ToolsBottomPanel.tsx
@@ -3,8 +3,8 @@
 *--------------------------------------------------------------------------------------------*/
 import * as React from "react";
 import classnames from "classnames";
-import { CoreTools } from "@itwin/appui-react";
-import { IModelApp, ToolSettings } from "@itwin/core-frontend";
+import { CoreTools, ToolItemDef } from "@itwin/appui-react";
+import { IModelApp, ToolSettings, ViewClipClearTool } from "@itwin/core-frontend";
 // import { MeasureToolDefinitions } from "@bentley/measure-tools-react";
 import {
   assignRef,
@@ -75,11 +75,22 @@ export function ToolsBottomPanel(props: ToolsBottomPanelProps) {
     // { labelKey: "ReactApp:ToolsBottomPanel.Angle", icon: "icon-measure-angle", toolItemDef: MeasureToolDefinitions.measureAngleToolCommand },
     // { labelKey: "ReactApp:ToolsBottomPanel.Perpendicular", icon: "icon-measure-perpendicular", toolItemDef: MeasureToolDefinitions.measurePerpendicularToolCommand },
     // { labelKey: "ReactApp:ToolsBottomPanel.Clear", icon: "icon-measure-clear", toolItemDef: MeasureToolDefinitions.clearMeasurementsToolCommand },
+    { labelKey: "ReactApp:ToolsBottomPanel.SectionByPlane", toolItemDef: CoreTools.sectionByPlaneCommandItemDef },
+    { labelKey: "ReactApp:ToolsBottomPanel.SectionByElement", toolItemDef: CoreTools.sectionByElementCommandItemDef },
+    { labelKey: "ReactApp:ToolsBottomPanel.SectionByRange", toolItemDef: CoreTools.sectionByRangeCommandItemDef },
+    { labelKey: "ReactApp:ToolsBottomPanel.SectionByShape", toolItemDef: CoreTools.sectionByShapeCommandItemDef },
+    { labelKey: "ReactApp:ToolsBottomPanel.ClearSection", icon: "icon-section-clear", toolItemDef: ToolItemDef.getItemDefForTool(ViewClipClearTool) },
   ];
 
   const activeToolId = useActiveToolId();
   const activeToolIndex = activeToolId !== undefined ? tools.findIndex((tool) => activeToolId === tool.toolItemDef.toolId) : undefined;
   const toolsRowRef = React.useRef<HTMLDivElement>(null);
+
+  // Use the virtual cursor for locating elements other than the select tool
+  // Any time activeToolId changes, ToolsBottomPanel() will execute again (due to useActiveToolId updating activeToolId).
+  // This allows us to put this here instead of inside the tool activation. If this is in the tool activation, it won't
+  // auto-detect when a tool ends on its own (switching back to select tool). The section tools all end on their own.
+  ToolSettings.enableVirtualCursorForLocate = activeToolId !== CoreTools.selectElementCommand.toolId;
 
   return <BottomPanel
     {...others}
@@ -95,15 +106,12 @@ export function ToolsBottomPanel(props: ToolsBottomPanelProps) {
           key={value.labelKey}
           className="tool-button"
           label={IModelApp.localization.getLocalizedString(value.labelKey)}
-          iconSpec={/* value.toolItemDef.iconSpec ??  */value.icon}
+          iconSpec={value.icon ?? value.toolItemDef.iconSpec}
           selected={activeToolId === value.toolItemDef.toolId}
           onClick={async () => {
             // Ensure the selectedView is the main viewport otherwise some tools won't execute as the selected view is incompatible.
             // This only applies when an app has more than one viewport, but does no harm.
             IModelApp.viewManager.setSelectedView(IModelApp.viewManager.getFirstOpenView());
-
-            // Use the virtual cursor for locating elements other than the select tool
-            ToolSettings.enableVirtualCursorForLocate = value.toolItemDef.toolId !== CoreTools.selectElementCommand.toolId;
 
             value.toolItemDef.execute();
             onToolClick?.();
