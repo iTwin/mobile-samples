86b86f3c1bcb5f92fc1f64aa19226b0946c2bb6d
Travis.Cobbs <77415528+tcobbs-bentley@users.noreply.github.com>
2022-01-13 15:02:09 -0800

    Update to require iOS 13.

--- iOS/MobileStarter/MobileStarter/AppDelegate.swift
+++ iOS/MobileStarter/MobileStarter/AppDelegate.swift
@@ -17,26 +17,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // Use our ModelApplication (a subclass of ITMApplication) as the application object.
         ITMViewController.application = ModelApplication()
-        if #available(iOS 13, *) {
-            // Delay the automatic loading of the frontend and backend to account for problem when
-            // that happens before the application's first willEnterForeground.
-            // Note that the sequence of events is different prior to iOS 13, so only do this in
-            // iOS 13 and later.
-            ITMViewController.delayedAutoLoad = true
-        }
+        // Delay the automatic loading of the frontend and backend to account for problem when
+        // that happens before the application's first willEnterForeground.
+        // Note that the sequence of events is different prior to iOS 13, so only do this in
+        // iOS 13 and later.
+        ITMViewController.delayedAutoLoad = true
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
-    @available(iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
-    @available(iOS 13.0, *)
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
