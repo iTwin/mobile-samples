09381b9a14c37a4fba3d06240fe94aa2a064949d
Travis Cobbs <77415528+tcobbs-bentley@users.noreply.github.com>
2022-02-02 13:17:51 -0800

    Move react-app to new top-level cross-platform directory. (#8)
    * Move react-app to new top-level cross-platform directory.
    
    * Remove development team that accidentally got committed.

--- iOS/CopyWebAppAssets.sh
+++ iOS/CopyWebAppAssets.sh
@@ -7,26 +7,27 @@ else
     WebAppDir=$1
 fi
 
-cd "${PROJECT_DIR}/react-app"
+reactAppDir=${PROJECT_DIR}/../../cross-platform/react-app
+cd "${reactAppDir}"
 ./GenITMAppConfig.sh
 cd -
 
 [ -d "$AppBundleRoot/$WebAppDir/frontend" ] || mkdir -p "$AppBundleRoot/$WebAppDir/frontend"
 [ -d "$AppBundleRoot/$WebAppDir/backend" ] || mkdir -p "$AppBundleRoot/$WebAppDir/backend"
-rsync -aL --delete "${PROJECT_DIR}/react-app/lib/webpack/" "$AppBundleRoot/$WebAppDir/backend/"
+rsync -aL --delete "${reactAppDir}/lib/webpack/" "$AppBundleRoot/$WebAppDir/backend/"
 
 # Enable this if your react-app has any backend assets (e.g. custom element properties) 
-rsync -aL --delete "${PROJECT_DIR}/react-app/assets/" "$AppBundleRoot/$WebAppDir/backend/assets/"
+rsync -aL --delete "${reactAppDir}/assets/" "$AppBundleRoot/$WebAppDir/backend/assets/"
 
 # must be done after rsync above
 [ -d "$AppBundleRoot/$WebAppDir/backend/assets" ] || mkdir -p "$AppBundleRoot/$WebAppDir/backend/assets"
-rsync -aL --delete "${PROJECT_DIR}/react-app/node_modules/@itwin/presentation-common/lib/cjs/assets/locales/" "$AppBundleRoot/$WebAppDir/backend/assets/locales/"
-rsync -aL --delete "${PROJECT_DIR}/react-app/node_modules/@itwin/presentation-backend/lib/cjs/assets/supplemental-presentation-rules/" "$AppBundleRoot/$WebAppDir/backend/assets/supplemental_presentation_rules/"
+rsync -aL --delete "${reactAppDir}/node_modules/@itwin/presentation-common/lib/cjs/assets/locales/" "$AppBundleRoot/$WebAppDir/backend/assets/locales/"
+rsync -aL --delete "${reactAppDir}/node_modules/@itwin/presentation-backend/lib/cjs/assets/supplemental-presentation-rules/" "$AppBundleRoot/$WebAppDir/backend/assets/supplemental_presentation_rules/"
 
-rsync -aL "${PROJECT_DIR}/react-app/ITMAppConfig.json" "$AppBundleRoot/$WebAppDir/"
-# if [ -f "${PROJECT_DIR}/react-app/ITMAppConfig.json" ]; then
-if [ grep -q baseUrl "${PROJECT_DIR}/react-app/ITMAppConfig.json" ]; then
+rsync -aL "${reactAppDir}/ITMAppConfig.json" "$AppBundleRoot/$WebAppDir/"
+# if [ -f "${reactAppDir}/ITMAppConfig.json" ]; then
+if [ grep -q baseUrl "${reactAppDir}/ITMAppConfig.json" ]; then
     rm -rf "$AppBundleRoot/$WebAppDir/frontend"
 else
-    rsync -aL --delete "${PROJECT_DIR}/react-app/build/" "$AppBundleRoot/$WebAppDir/frontend/"
+    rsync -aL --delete "${reactAppDir}/build/" "$AppBundleRoot/$WebAppDir/frontend/"
 fi
--- iOS/README.md
+++ iOS/README.md
@@ -51,10 +51,10 @@ Before building the samples, you must configure a Client ID for yourself. To do 
 1. You must install node 14 on your Mac, and it needs to be the default node found in your path.
 1. If you are using a Mac with Apple Silicon, you must do the following:
     1. Run a Rosetta 2 Terminal.
-    1. cd into the `iOS/MobileStarter/react-app` directory of this repository.
+    1. cd into the `cross-platform/react-app` directory of this repository.
     1. Run `npm install`.
 1. Run a Terminal
-1. cd into the `iOS/MobileStarter/react-app` directory of this repository.
+1. cd into the `cross-platform/react-app` directory of this repository.
 1. If you are not on a Mac with Apple Silicon and did so above, run `npm install`.
 1. Run `npm run build`.
 1. Run `npm run start`. __Note:__ This starts a React Debug Server on your Mac that the app will communicate with. It must be running when you run the app. This requires that the device be able to connect to your Mac. If you want the app to run purely on the device (like it would if it were released), uncomment the `ITMAPPLICATION_NO_DEBUG_SERVER = YES` line in iOSSamples.xcconfig. If you do this, there is no need for the React Debug Server to be running when you run the app. (If you open the react-app directory in Visual Studio Code, it is configured such that Cmd+Shift+B will perform the `npm run start` in an integrated terminal.)
