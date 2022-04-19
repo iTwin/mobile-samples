1652efc3ad0fdd8b440fa4e3c77e4c34ecec7f42
Travis Cobbs <77415528+tcobbs-bentley@users.noreply.github.com>
2022-04-19 13:46:41 -0700

    Temporarily put Building Blocks.bim into Android sample (#44)

---
+++ Android/iTwinStarter/app/src/main/java/com/bentley/sample/itwinstarter/DocumentPicker.kt
@@ -0,0 +1,20 @@
+package com.bentley.sample.itwinstarter
+
+import android.content.Context
+import android.webkit.WebView
+import com.eclipsesource.json.Json
+import com.github.itwin.mobilesdk.ITMCoMessenger
+import com.github.itwin.mobilesdk.ITMComponent
+import java.io.File
+
+class DocumentPicker(context: Context, webView: WebView, coMessenger: ITMCoMessenger): ITMComponent(context, webView, coMessenger) {
+    init { listener = coMessenger.addQueryListener("chooseDocument") {
+            var bimFile = File(context.filesDir, "ITMApplication/backend/Building Blocks.bim")
+            if (bimFile.exists()) {
+                Json.value(bimFile.absolutePath)
+            } else {
+                Json.value("")
+            }
+        }
+    }
+}
--- Android/iTwinStarter/app/src/main/java/com/bentley/sample/itwinstarter/ModelApplication.kt
+++ Android/iTwinStarter/app/src/main/java/com/bentley/sample/itwinstarter/ModelApplication.kt
@@ -1,5 +1,7 @@
 package com.bentley.sample.itwinstarter
 import android.net.Uri
+import android.os.Messenger
+import com.eclipsesource.json.Json
 import com.github.itwin.mobilesdk.ITMApplication
 //import com.bentley.itwin.AuthorizationClient
 
@@ -9,5 +11,15 @@ object ModelApplication : ITMApplication(StarterApplication.getContext(), BuildC
 
     override fun openUri(uri: Uri) {
         MainActivity.openUri(uri)
+    }
+
+    override fun setupWebView() {
+        super.setupWebView()
+        coMessenger?.let { coMessenger ->
+            coMessenger.addMessageListener("loading") {}
+            coMessenger.addQueryListener("getBimDocuments") {
+                Json.array()
+            }
+        }
     }
 }
