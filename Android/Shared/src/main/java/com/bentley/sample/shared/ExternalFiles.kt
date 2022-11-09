0d9549c66660d174e19df41b8ca4ef2575f56ff3
Todd Southen <48103957+toddsouthenbentley@users.noreply.github.com>
2022-11-09 14:35:06 -0800

    Renamed function

--- Android/Shared/src/main/java/com/bentley/sample/shared/ExternalFiles.kt
+++ Android/Shared/src/main/java/com/bentley/sample/shared/ExternalFiles.kt
@@ -15,7 +15,7 @@ import java.io.File
  * @param uri The input content Uri.
  * @return The display name or null if it wasn't found.
  */
-fun ContentResolver.getFileDisplayName(uri: Uri): String? {
+fun ContentResolver.getDisplayName(uri: Uri): String? {
     // Query the content resolver only if we have a content Uri
     return uri.takeIf { uri.scheme == ContentResolver.SCHEME_CONTENT }?.let {
         query(uri, arrayOf(OpenableColumns.DISPLAY_NAME), null, null, null, null)?.use { cursor ->
@@ -31,7 +31,7 @@ fun ContentResolver.getFileDisplayName(uri: Uri): String? {
  * @return The full path of the copied file, null if it failed.
  */
 fun Context.copyToExternalFiles(uri: Uri, destDir: String): String? {
-    return contentResolver.getFileDisplayName(uri)?.let { displayName ->
+    return contentResolver.getDisplayName(uri)?.let { displayName ->
         getExternalFilesDir(null)?.let { filesDir ->
             contentResolver.openInputStream(uri)?.use { input ->
                 val outDir = File(filesDir.path, destDir)
