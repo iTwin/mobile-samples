351129f4e5e39c47f3028fbbfd01e8e336390220
Travis Cobbs <77415528+tcobbs-bentley@users.noreply.github.com>
2022-12-01 12:11:58 -0800

    Optionally log startup times in Android samples (#150)

--- Android/Shared/src/main/java/com/bentley/sample/shared/SampleITMApplication.kt
+++ Android/Shared/src/main/java/com/bentley/sample/shared/SampleITMApplication.kt
@@ -6,14 +6,25 @@ package com.bentley.sample.shared
 
 import android.content.Context
 import android.net.Uri
+import androidx.fragment.app.FragmentActivity
 import com.eclipsesource.json.Json
 import com.github.itwin.mobilesdk.ITMApplication
+import com.github.itwin.mobilesdk.jsonvalue.isYes
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 
 /**
  * The base [ITMApplication] implementation for the sample applications.
  * Applications should sub-class using a singleton object and call [finishInit] in the init block.
  */
 open class SampleITMApplication(context: Context, attachWebViewLogger: Boolean, forceExtractBackendAssets: Boolean) : ITMApplication(context, attachWebViewLogger, forceExtractBackendAssets) {
+    private val startupTimer = ActivityTimer()
+
+    override fun finishInit() {
+        super.finishInit()
+        startupTimer.enabled = configData?.isYes("ITMSAMPLE_LOG_STARTUP_TIMES") ?: true
+    }
+
     /**
      * Implements the abstract function by delegating to [MainActivity].
      */
@@ -26,9 +37,13 @@ open class SampleITMApplication(context: Context, attachWebViewLogger: Boolean, 
      */
     override fun setupWebView() {
         super.setupWebView()
-        coMessenger.registerMessageHandler("loading") {}
+        coMessenger.registerMessageHandler("loading") {
+            startupTimer.addCheckpoint("Webview load")
+        }
         coMessenger.registerMessageHandler("didFinishLaunching") {
             coMessenger.frontendLaunchSucceeded()
+            startupTimer.addCheckpoint("Launch total")
+            startupTimer.logTimes(logger,"STARTUP TIMES")
         }
 
         coMessenger.registerQueryHandler("getBimDocuments") {
@@ -64,5 +79,30 @@ open class SampleITMApplication(context: Context, attachWebViewLogger: Boolean, 
      */
     fun onResume() {
         this.host?.onResume()
+    }
+
+    override fun initializeBackend(
+        fragmentActivity: FragmentActivity,
+        allowInspectBackend: Boolean
+    ) {
+        startupTimer.addCheckpoint("Before backend load")
+        super.initializeBackend(fragmentActivity, allowInspectBackend)
+        MainScope().launch {
+            waitForBackendInitialize()
+            startupTimer.addCheckpoint("After backend load")
+        }
+    }
+
+    override fun initializeFrontend(
+        fragmentActivity: FragmentActivity,
+        fragmentContainerId: Int,
+        allowInspectBackend: Boolean
+    ) {
+        startupTimer.addCheckpoint("Before frontend load")
+        super.initializeFrontend(fragmentActivity, fragmentContainerId, allowInspectBackend)
+        MainScope().launch {
+            waitForFrontendInitialize()
+            startupTimer.addCheckpoint("After frontend load")
+        }
     }
 }
