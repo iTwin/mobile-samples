4fd69634909bb56675ce1fe8e6505ef7396ea8ab
Travis Cobbs <77415528+tcobbs-bentley@users.noreply.github.com>
2022-12-05 14:15:22 -0800

    Add key to ToolsBottomPanel in TabBar (#153)

--- cross-platform/react-app/src/frontend/Screens/ModelScreen.tsx
+++ cross-platform/react-app/src/frontend/Screens/ModelScreen.tsx
@@ -250,6 +250,7 @@ export function ModelScreen(props: ModelScreenProps) {
       isTab: true,
       popup: React.createElement(toolsBottomPanel ?? ToolsBottomPanel,
         {
+          key: "tools",
           iModel,
           // Close the Views bottom panel when a view is selected from it.
           onToolClick: () => { tabsAndPanelsAPI.closeSelectedPanel(); },
