33a2f8bfb26c0882ff83a2706a83c8ff76f5b383
Travis.Cobbs <77415528+tcobbs-bentley@users.noreply.github.com>
2023-04-25 16:14:25 -0700

    Code cleanup: * Move ActiveScreen def to Screen.tsx * Add doc comments * Remove workaround for formerly broken useTheme("os") * Add home icon * Resize folder icon

--- cross-platform/react-app/src/frontend/Components/Screen.tsx
+++ cross-platform/react-app/src/frontend/Components/Screen.tsx
@@ -7,8 +7,20 @@ import classnames from "classnames";
 import { IModelApp } from "@itwin/core-frontend";
 import { Messenger, presentAlert } from "@itwin/mobile-sdk-core";
 import { useTheme } from "@itwin/itwinui-react";
-import { useActiveColorSchemeIsDark } from "@itwin/mobile-ui-react";
 import "./Screen.scss";
+
+/**
+ * The App's active screen
+ *
+ * Note: Putting this in App.tsx leads to circular imports.
+ */
+export enum ActiveScreen {
+  Loading,
+  Home,
+  LocalModels,
+  Hub,
+  Model,
+}
 
 /** Properties for the {@link Screen} React component. */
 export interface ScreenProps {
@@ -17,6 +29,14 @@ export interface ScreenProps {
   children?: React.ReactNode;
 }
 
+/**
+ * Look up a localized string in the `ReactApp` i18n namespace.
+ *
+ * @param prefix The prefix of (top-level group) for the localized string.
+ * @param key The name of the localized string.
+ * @param options Optional options to pass to `getLocalizedString`.
+ * @returns The given localized string.
+ */
 export function i18n(prefix: string, key: string, options?: any) {
   if (window.itmSampleParams.debugI18n) {
     return `=${IModelApp.localization.getLocalizedString(`ReactApp:${prefix}.${key}`, options)}=`;
@@ -25,6 +45,13 @@ export function i18n(prefix: string, key: string, options?: any) {
   }
 }
 
+/**
+ * Convert a number to a string, rounded to a requested number of decimal places.
+ *
+ * @param input The number to round.
+ * @param decimals The number of decimal places to round to, default 2.
+ * @returns A string representation of {@link input}, rounded to the requested decimal places.
+ */
 export function roundedNumber(input: number, decimals?: number) {
   if (decimals === undefined) {
     decimals = 2;
@@ -43,6 +70,13 @@ export function roundedNumber(input: number, decimals?: number) {
   return rounded;
 }
 
+/**
+ * Convert a number representing a file size to a human-readable format.
+ *
+ * @param input The size of the file.
+ * @param decimals The number of decimal places to round to, default 2.
+ * @returns A human-readable representation of the file size, like "42.89 MB".
+ */
 export function fileSizeString(input?: number, decimals?: number) {
   if (input === undefined) {
     return i18n("Screen", "MBFormat", { size: "?" });
@@ -64,15 +98,22 @@ export function fileSizeString(input?: number, decimals?: number) {
 
 /** React component for a simple full-screen UI with arbitrary children. */
 export function Screen(props: ScreenProps = {}) {
-  const isDark = useActiveColorSchemeIsDark();
   const { className, children } = props;
 
-  // The useTheme hook below does not currently detect theme changes on the fly if "os" is
-  // set as the theme.
-  useTheme(isDark ? "dark" : "light");
+  useTheme("os");
   return <div className={classnames("screen", className)}>{children}</div>;
 }
 
+/**
+ * Show an alert box for the given error using {@link presentAlert}.
+ *
+ * __Note__: Even though {@link presentAlert} is async, this function does not wait for it to complete.
+ *
+ * @param formatKey The localization key for the format string used to describe the error.
+ * @param error The error that is being presented.
+ * @param namespace The i18n namespace for {@link formatKey}, default "App".
+ * @param showStatusBar Whether or not the device status bar should be visible while displaying the alert, default `true`.
+ */
 export function presentError(formatKey: string, error: any, namespace = "App", showStatusBar = true) {
   const errorMessage = (error instanceof Error) ? error.message : error;
   void presentAlert({
@@ -86,6 +127,9 @@ export function presentError(formatKey: string, error: any, namespace = "App", s
   });
 }
 
+/**
+ * Sign out of OIDC, presenting an error if there is a problem doing that.
+ */
 export async function signOut() {
   try {
     await Messenger.query("signOut");
