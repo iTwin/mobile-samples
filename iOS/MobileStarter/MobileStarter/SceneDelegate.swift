3dae8fc4835e9d8dc13362b350ae6debea28e56b
Travis.Cobbs <77415528+tcobbs-bentley@users.noreply.github.com>
2024-02-29 12:47:11 -0800

    Fix openInboxURL in samples.

--- iOS/MobileStarter/MobileStarter/SceneDelegate.swift
+++ iOS/MobileStarter/MobileStarter/SceneDelegate.swift
@@ -30,7 +30,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
         if let url = URLContexts.first?.url {
-            DocumentHelper.openInboxUrl(url)
+            DocumentHelper.openInboxURL(url)
         }
     }
     
