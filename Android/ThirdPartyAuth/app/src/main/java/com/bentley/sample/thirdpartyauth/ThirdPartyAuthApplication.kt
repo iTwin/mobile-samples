0fc145d8df1adb4f1ceedf21d4aef4cad15fea53
Travis Cobbs <77415528+tcobbs-bentley@users.noreply.github.com>
2024-03-21 12:57:56 -0700

    Fix problems with Android ThirdPartyAuth (#312)

--- Android/ThirdPartyAuth/app/src/main/java/com/bentley/sample/thirdpartyauth/ThirdPartyAuthApplication.kt
+++ Android/ThirdPartyAuth/app/src/main/java/com/bentley/sample/thirdpartyauth/ThirdPartyAuthApplication.kt
@@ -3,21 +3,22 @@ package com.bentley.sample.thirdpartyauth
 import android.app.Application
 import android.content.Context
 import com.bentley.sample.shared.MainActivity
+import com.bentley.sample.shared.SampleApplicationBase
 
-class ThirdPartyAuthApplication : Application() {
+class ThirdPartyAuthApplication : SampleApplicationBase<ThirdPartyAuthITMApplication>(ThirdPartyAuthITMApplication::newInstance) {
 
     lateinit var loginViewModel: LoginViewModel
         private set
 
     /**
-     * Sets [MainActivity.sampleITMApplication] to [ThirdPartyAuthITMApplication] so that our auth handler is setup.
+     * Sets [MainActivity.sampleITMApplication] to [ThirdPartyAuthITMApplication] so that our auth
+     * handler is set up.
      */
     override fun onCreate() {
         super.onCreate()
         appContext = applicationContext
-        MainActivity.sampleITMApplication = ThirdPartyAuthITMApplication
         loginViewModel = LoginViewModel(ResourceHelper(this)) { credentialsManager ->
-            ThirdPartyAuthITMApplication.auth0CredentialsManager = credentialsManager
+            itmApplication.auth0CredentialsManager = credentialsManager
         }
     }
 
--- Android/ThirdPartyAuth/app/src/main/java/com/bentley/sample/thirdpartyauth/ThirdPartyAuthITMApplication.kt
+++ Android/ThirdPartyAuth/app/src/main/java/com/bentley/sample/thirdpartyauth/ThirdPartyAuthITMApplication.kt
@@ -1,24 +1,28 @@
 package com.bentley.sample.thirdpartyauth
 
-import androidx.fragment.app.FragmentActivity
+import android.content.Context
 import com.auth0.android.authentication.storage.CredentialsManager
 import com.bentley.itwin.AuthorizationClient
 import com.bentley.sample.shared.SampleITMApplication
 
-object ThirdPartyAuthITMApplication : SampleITMApplication(
-    ThirdPartyAuthApplication.getContext(),
+class ThirdPartyAuthITMApplication(context: Context) : SampleITMApplication(
+    context,
     BuildConfig.DEBUG,
     BuildConfig.DEBUG
 ) {
+    companion object {
+        fun newInstance(context: Context) = ThirdPartyAuthITMApplication(context)
+    }
+
     var auth0CredentialsManager: CredentialsManager? = null
 
     init {
         finishInit()
     }
 
-    override fun createAuthorizationClient(fragmentActivity: FragmentActivity): AuthorizationClient? {
+    override fun createAuthorizationClient(): AuthorizationClient? {
         return auth0CredentialsManager?.let {
-            TokenServerAuthClient(fragmentActivity.getString(R.string.ITMSAMPLE_TOKEN_SERVER_URL), it)
+            TokenServerAuthClient(appContext.getString(R.string.ITMSAMPLE_TOKEN_SERVER_URL), it)
         }
     }
 }
