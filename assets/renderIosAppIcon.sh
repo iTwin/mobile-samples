9b37ad3d4905cf9fdd082d500f39dabdd996e96f
Travis Cobbs <77415528+tcobbs-bentley@users.noreply.github.com>
2025-01-16 16:21:54 -0500

    Update iOS app icons to support current sizes plus dark mode (#358)
    Also commit script that generates iOS icons at appropriate sizes

---
+++ assets/renderIosAppIcon.sh
@@ -0,0 +1,35 @@
+#! /bin/zsh
+
+function render_size
+{
+  if [ "${3}" = "" ]; then
+    size_label=$1
+  else
+    size_label=$3
+  fi
+  if [[ "$2" =~ ".*Dark.*" ]]; then
+    # qlmanage doesn't support transparent background
+    # This requires rsvg-convert to be installed and in the path. With homebrew,
+    # you can install it with `brew install librsvg`
+    rsvg-convert -w $1 -h $1 "$2" > "${2}.png"
+  else
+    qlmanage -t -s $1 -o . "$2"
+  fi
+  mv "${2}.png" "${2%.svg}-${size_label}.png"
+}
+render_size 40 $1 "20@2x"
+render_size 58 $1 "29@2x"
+render_size 60 $1 "20@3x"
+render_size 76 $1 "38@2x"
+render_size 114 $1 "38@3x"
+render_size 80 $1 "40@2x"
+render_size 87 $1 "29@3x"
+render_size 120 $1 "40@3x"
+render_size 120 $1 "60@2x"
+render_size 128 $1 "64@2x"
+render_size 192 $1 "64@3x"
+render_size 136 $1 "68@2x"
+render_size 152 $1 "76@2x"
+render_size 167 $1 "83.5@2x"
+render_size 180 $1 "60@3x"
+render_size 1024 $1
